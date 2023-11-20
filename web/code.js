dfc2eeb38eb07a57a0da8b0661b28b65ea89f100
Michel Thies <michelthies@Michels-MBP-2.fritz.box>
2023-11-21 00:00:56 +0100

    updated html structure and added custom styling

--- web/code.js
+++ web/code.js
@@ -1,14 +1,18 @@
 /* jshint esversion: 6 */
+document.addEventListener('DOMContentLoaded', function() {
 
-document.getElementById('searchbutton').onclick = () => {
-    fetch("/search?q=" + document.getElementById('searchbox').value)
-    .then((response) => response.json())
-    .then((data) => {
-        document.getElementById("responsesize").innerHTML = 
-            "<p>" + data.length + " websites retrieved</p>";
-        let results = data.map((page) =>
-            `<li><a href="${page.url}">${page.title}</a></li>`)
-            .join("\n");
-        document.getElementById("urllist").innerHTML = `<ul>${results}</ul>`;
-    });
-};
+    document.getElementById('searchbutton').onclick = () => {
+        fetch("/search?q=" + document.getElementById('searchbox').value)
+        .then((response) => response.json())
+        .then((data) => {
+            document.getElementById("responsesize").innerHTML = 
+                "<p>" + data.length + " websites retrieved</p>";
+            let results = data.map((page) =>
+                `<li><a class="content-anchor" href="${page.url}">${page.title}</a></li>`)
+                .join("\n");
+            document.getElementById("urllist").innerHTML = `<ul>${results}</ul>`;
+        });
+    };
+
+
+});
